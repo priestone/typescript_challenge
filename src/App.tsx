d83026c73da7776c1e35fedd042b04d45283b13a
preistone <aion97@naver.com>
2025-01-11 20:03:55 +0900

    styled components setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-const App = () => {
+import React from "react";
+import styled from "styled-components";
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: black;
+  color: white;
+`;
+
+const App: React.FC = () => {
   return (
     <>
-      <h2>프링글스</h2>
+      <Box>프링글스</Box>
     </>
   );
 };
